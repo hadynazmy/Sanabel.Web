70ead5c9e791f91ead775ce0fb1eaf53ab43fc21
Your Name <you@example.com>
2025-04-30 11:00:03 +0300

    تم الانتهاء من الصلاحيات و المستخدمين

---
+++ Sanabel.Web/ViewModels/RoleFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sanabel.Web.ViewModels
+{
+    public class RoleFormViewModel
+    {
+        [Required, MaxLength(256)]
+        public string Name { get; set; }
+    }
+}
